816e2d776812c0d6928162c07bad27a9e63d9f0b
Yan Chagas <yanchagas2004@gmail.com>
2025-04-29 17:51:49 -0300

    reverting changes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-const serverless = require('serverless-http')
 const { app } = require('../src/app');
 
-const handler = serverless(app, { provider: 'azure' });
-module.exports.funcName = async (context, req) => {
-  context.res = await handler(context, req);
-}
+module.exports = app;
