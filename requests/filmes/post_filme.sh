4a82359c38e9c6142c70dddda48edce12fc6c4b8
yanchagasPC <yanchagas2004@gmail.com>
2025-05-10 14:35:05 -0300

    fix: Colocando a url do vercel nos requests

--- requests/filmes/post_filme.sh
+++ requests/filmes/post_filme.sh
@@ -1,5 +1,5 @@
 curl --request POST \
-  --url http://localhost:3000/api/filmes \
+  --url https://atividade-iv-v1-full-stack-six.vercel.app/api/filmes \
   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MWUxMWZkYmUwYzA4NTQxN2EwMGI0YSIsIm5hbWUiOiJKb2huIERvZSIsImVtYWlsIjoiam9obkBleGFtcGxlLmNvbSIsImlhdCI6MTc0NjgwMTMyMCwiZXhwIjoxNzQ5MzkzMzIwfQ.dbU8nTsNXfnF3OBPO-uxs31VFAVvSyTqJhZX2CyuS-s' \
   --header 'Content-Type: application/json' \
   --data '{
--- requests/filmes/put_filme.sh
+++ requests/filmes/put_filme.sh
@@ -1,5 +1,5 @@
 curl --request PUT \
-  --url http://localhost:3000/api/filmes/ce3d05e0-570f-4247-8fc0-1583ae1b6cf8 \
+  --url https://atividade-iv-v1-full-stack-six.vercel.app/api/filmes/ce3d05e0-570f-4247-8fc0-1583ae1b6cf8 \
   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MWUxMWZkYmUwYzA4NTQxN2EwMGI0YSIsIm5hbWUiOiJKb2huIERvZSIsImVtYWlsIjoiam9obkBleGFtcGxlLmNvbSIsImlhdCI6MTc0NjgwMTMyMCwiZXhwIjoxNzQ5MzkzMzIwfQ.dbU8nTsNXfnF3OBPO-uxs31VFAVvSyTqJhZX2CyuS-s' \
   --header 'Content-Type: application/json' \
   --data '{
